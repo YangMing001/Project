0305477febc678b5f1ae95239aa493a9f25c3e28
YM <yangming@1yd.me>
2016-06-08 14:56:39 +0800

    rm

---
+++ Project/Pods/Headers/Private/Test1/YMHelper.h
@@ -0,0 +1,1 @@
+../../../Test1/Test1/Classes/YMHelper.h
---
+++ Project/Pods/Test1/Test1/Classes/YMHelper.m
@@ -0,0 +1,17 @@
+//
+//  YMHelper.m
+//  Test1
+//
+//  Created by YM on 16/6/8.
+//  Copyright © 2016年 YM. All rights reserved.
+//
+
+#import "YMHelper.h"
+
+@implementation YMHelper
+
++ (void)log{
+    NSLog(@"---不容易--");
+}
+
+@end
